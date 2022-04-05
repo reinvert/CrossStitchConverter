2d7bda009d5359771115644515fec028a792ef10
USER <USER@DESKTOP-S2DNBJ9>
2022-04-05 10:37:27 +0900

    Markup cleaned

--- src/module-info.java
+++ src/module-info.java
@@ -10,7 +10,6 @@ module CrossStitchConverter {
 	exports com.stitch.converter.view;
 	exports com.stitch.converter;
 
-	requires transitive java.desktop;
 	requires transitive javafx.base;
 	requires transitive javafx.controls;
 	requires transitive javafx.graphics;
